f53f32c9153eacc7d02aa2016269176344211efe
mart10angel <163224416+mart10angel@users.noreply.github.com>
2024-05-03 16:14:33 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para agregar un producto al carrito
+function addToCart(productName, productPrice) {
+    // Aquí puedes implementar la lógica para agregar el producto al carrito
+    alert(`¡${productName} agregado al carrito por $${productPrice}!`);
+}
